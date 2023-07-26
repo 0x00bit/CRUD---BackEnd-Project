d35a55a6d78414879f43cf7bddbcc6541ccc768b
magnstr <136848260+magnstr@users.noreply.github.com>
2023-07-26 17:01:26 -0300

    Add files via upload

--- cadastros.php
+++ cadastros.php
@@ -1,25 +1,23 @@
 <?php
+
     if(isset($_POST['nome'])){
-        $nome = $_POST['nome'];
+        $conta = $_POST['nome'];
     }
-    if(isset($_POST['altura'])){
-        $altura = $_POST['altura'];
+    if(isset($_POST['empresa'])){
+        $empresa = $_POST['empresa'];
     }
-    if(isset($_POST['nacionalidade'])){
-        $nacionalidade = $_POST['nacionalidade'];
+    if(isset($_POST['banco'])){
+        $banco = $_POST['banco'];
     }
-    if(isset($_POST['sexo'])){
-        $sexo = $_POST['sexo'];
+    if(isset($_POST['agencia'])){
+        $agencia = $_POST['agencia'];
     }
-    if(isset($_POST['nascimento'])){
-        $nascimento = $_POST['nascimento'];
-    }
-    if(isset($_POST['peso'])){
-        $peso = $_POST['peso'];
+    if(isset($_POST['contac'])){
+        $contac = $_POST['contac'];
     }
 
-    try{
-        $pdo = new PDO("mysql:dbname=cadastros;host=localhost;","root","");
+    /*try{
+        $pdo = new PDO("mysql:dbname=registros;host=localhost;","root","");
     }catch(PDOExeception $e){
         echo "Erro ao tentar se conectar com o banco de dados!".$e->getMessage();
     }
@@ -28,10 +26,10 @@
     }
     //DB name >> host >> user >> password
 
-    $payload = $pdo->prepare("INSERT INTO clients(nome, altura, nacionalidade, sexo, nascimento, peso)
+    $payload = $pdo->prepare("INSERT INTO funcionarios(nome, empresa, banco, agencia, contac)
     VALUES (?,?,?,?,?,?)");
 
-    $payload->execute([$nome,$altura,$nacionalidade,$sexo,$nascimento,$peso]);
+    $payload->execute([$nome,$altura,$nacionalidade,$sexo,$nascimento,$peso]);*/
     
 ?>
 
@@ -40,6 +38,7 @@
 <head>
     <meta charset="utf-8">
     <title>Cadastros</title>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <style>
         body {
             font-family: 'Poppins', sans-serif;
@@ -82,33 +81,34 @@
             cursor: pointer;
             font-family: 'Poppins', sans-serif;
         }
+        h3{
+            text-align: center;
+        }
+        #btn{
+            font-family: 'Poppins';
+        }
     </style>
 </head>
 <body>
     <h1>Portal de Cadastramento</h1>
+    <h3>Cadastros de contas salário - Folha de funcionários</h3>
     <form action="cadastros.php" name="cadastro" method="POST">
-        <label for="nome">Nome:</label>
-        <input type="text" name="nome" id="nome" required>
+        <label for="Conta">Nome</label>
+        <input type="text" name="nome" required>
         
-        <label for="altura">Altura:</label>
-        <input type="text" name="altura" id="altura" required>
+        <label for="firma">Empresa</label>
+        <input type="text" name="empresa" required>
         
-        <label for="nacionalidade">Nacionalidade:</label>
-        <input type="text" name="nacionalidade" id="nacionalidade" required>
+        <label for="banco">Nome do banco</label>
+        <input type="text" name="banco" required>
         
-        <label for="sexo">Sexo:</label>
-        <input type="radio" name="sexo" id="sexo-m" value="M" required>
-        <label for="sexo-m">M</label>
-        <input type="radio" name="sexo" id="sexo-f" value="F" required>
-        <label for="sexo-f">F</label>
+        <label for="agencia">Agência</label>
+        <input type="text" name="agencia" required>
         
-        <label for="nascimento">Nascimento:</label>
-        <input type="date" name="nascimento" id="nascimento" required>
+        <label for="peso">Conta corrente</label>
+        <input type="text" name="contac" required>
         
-        <label for="peso">Peso:</label>
-        <input type="text" name="peso" id="peso" required>
-        
-        <input type="submit" value="Enviar">
+        <input type="submit" value="Enviar" id="btn">
     </form>
 
 </body>
